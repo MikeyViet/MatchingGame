3057aca3bade20c01b0dfccdfceef6e3f5453834
cuviet <vietwiin@gmail.com>
2022-06-30 14:08:20 -0500

    Added Icons to board

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,45 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        // Use this random object to choose random icons for the square
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon will appear twice in the list
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        /// <summary>
+        ///  Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
         }
     }
 }
